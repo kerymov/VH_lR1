ddb7d8f3f0443c4622cdde7e77b0b539dd2c5741
Анастасія <@DESKTOP-1HQ9ES4>
2025-03-08 03:15:14 +0200

    Add project files.

---
+++ VH_lR1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VH_lR1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
